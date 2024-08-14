3e6be66a14e6fa336a65e95bc1e57a9d7b1e0111
Lexi Cro <justlexcroftw@gmail.com>
2024-08-14 16:53:31 +0300

    Created the Employee repo and add it to IUnitOfWork

---
+++ InventarVali.DataAccess/Repository/EmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿using InventarVali.DataAccess.Data;
+using InventarVali.DataAccess.Repository.IRepository;
+using InventarVali.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarVali.DataAccess.Repository
+{
+    public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
+    {
+        private ApplicationDbContext _db;
+        public EmployeeRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+        public void Update(Employee obj)
+        {
+            _db.Employees.Update(obj);
+        }
+    }
+}
--- InventarVali.DataAccess/Repository/IRepository/IEmployeeRepository.cs
+++ InventarVali.DataAccess/Repository/IRepository/IEmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InventarVali.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,8 @@ using System.Threading.Tasks;
 
 namespace InventarVali.DataAccess.Repository.IRepository
 {
-    public interface IUnitOfWork
+    public interface IEmployeeRepository : IRepository<Employee>
     {
-        IGoodsRepository Goods { get; }
-        IEmployeeRepository Employee { get; }
-        void Save();
+        void Update(Employee obj);
     }
 }
