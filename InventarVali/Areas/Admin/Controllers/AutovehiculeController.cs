68379ca4e3a0efa13dcafbf07b6df67379191f14
Lexi Cro <justlexcroftw@gmail.com>
2024-08-27 11:45:24 +0300

    Created the Upsert Method

--- InventarVali/Areas/Admin/Controllers/AutovehiculeController.cs
+++ InventarVali/Areas/Admin/Controllers/AutovehiculeController.cs
@@ -21,7 +21,7 @@ namespace InventarVali.Areas.Admin.Controllers
             return View(objAutovehiculeslist);
         }
 
-        public IActionResult CreateAutovehicule() 
+        public IActionResult Upsert(int? id) 
         {
             AutovehiculeVM autovehiculeVM = new()
             {
@@ -32,10 +32,22 @@ namespace InventarVali.Areas.Admin.Controllers
                 }),
                 Autovehicule = new Autovehicule()
             };
-            return View(autovehiculeVM);
+
+            if (id == null || id == 0)
+            {
+                //Create
+                return View(autovehiculeVM);
+            }
+            else 
+            {
+                //Update
+                autovehiculeVM.Autovehicule = _unitOfWork.Autovehicule.Get(o => o.Id == id);
+                return View(autovehiculeVM);
+            }
+            
         }
         [HttpPost]
-        public IActionResult CreateAutovehicule(AutovehiculeVM autovehiculeVM) 
+        public IActionResult Upsert(AutovehiculeVM autovehiculeVM, IFormFile? file) 
         {
             if (ModelState.IsValid)
             {
@@ -56,35 +68,6 @@ namespace InventarVali.Areas.Admin.Controllers
             }
         }
 
-        public IActionResult Edit(int id) 
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Autovehicule? obj = _unitOfWork.Autovehicule.Get(x => x.Id == id);
-            if (obj == null) 
-            {
-                return NotFound();
-            }
-            return View(obj);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Autovehicule obj) 
-        {
-            
-            if (ModelState.IsValid) 
-            {
-                _unitOfWork.Autovehicule.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "The Autovehicule was created successfully";
-
-                return RedirectToAction("Index", "Autovehicule");
-            }
-            return View();
-        }
-
         public IActionResult Delete(int id) 
         {
             if (id == null || id == 0) 
@@ -101,6 +84,7 @@ namespace InventarVali.Areas.Admin.Controllers
 
             return View(obj);
         }
+
         [HttpPost, ActionName("Delete")]
         public IActionResult DeleteVehicule(int id) 
         {
