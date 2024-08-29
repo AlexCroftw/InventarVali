14e096eb1ade7cb31ebf1127a92dd857115b14b4
Lexi Cro <justlexcroftw@gmail.com>
2024-08-29 16:36:33 +0300

    Compleated the JS for TableData and resolved a bugg where Ajax would not call on the API Route provided

--- InventarVali/wwwroot/js/computer.js
+++ InventarVali/wwwroot/js/computer.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-    $('#tblData').DataTable();
+    loadDataTable();
 });
 
 function loadDataTable() {
@@ -7,14 +7,25 @@ function loadDataTable() {
         "ajax": {
             url: '/admin/computer/getall'
         },
-        "colums": [
+        "columns": [
             { data: 'type', "width": "15%" },
             { data: 'model', "width": "15%" },
             { data: 'description', "width": "15%" },
             { data: 'serialNumber', "width": "15%" },
             { data: 'imageUrl', "width": "15%" },
-            { data: 'employees.fullName', "width": "15%" }
+            { data: 'employees.fullName', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                      <a href = "/admin/computer/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit> </a>
+                      <a href = "/admin/computer/delete?id=${data}" class="btn btn-danger mx-2">  <i class="bi bi-trash3"></i>Delete </a>
+                           </div>`
+                },
+                "width": "25%"
+            }
         ]
     })
 
 }
+
