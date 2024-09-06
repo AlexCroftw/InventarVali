a919079321c5a9920a76ee27179e76b5897ac0f7
Lexi Cro <justlexcroftw@gmail.com>
2024-09-06 17:14:31 +0300

    Changed HTML code for Employee as well

--- InventarVali/wwwroot/js/employee.js
+++ InventarVali/wwwroot/js/employee.js
@@ -17,7 +17,7 @@ function loadDataTable() {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                      <a href = "/admin/employee/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"> </i>Edit> </a>
+                      <a href = "/admin/employee/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"> </i> Edit </a>
                       <a onClick = Delete('/admin/employee/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash3"> </i> Delete </a>
                            </div>`
                 },
