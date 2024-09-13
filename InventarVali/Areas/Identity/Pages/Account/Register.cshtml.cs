fcb30a1fc4afe6ac7d597833bddd0d91f892bb9e
Lexi Cro <justlexcroftw@gmail.com>
2024-09-13 15:12:56 +0300

    Made changes so that only Admin can create a Admin Role

--- InventarVali/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ InventarVali/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -170,7 +170,14 @@ namespace InventarVali.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New User Created Successfully";
+                        }
+                        else 
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }                      
                         return LocalRedirect(returnUrl);
                     }
                 }
