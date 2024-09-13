c3e00d9337323f27ab0db62c9c3f5e261a2ccd08
Your Name <you@example.com>
2024-09-13 21:20:32 +0300

    Solved a visual bugg where the Edit button had an extra "<"

--- InventarVali/wwwroot/js/autovehicule.js
+++ InventarVali/wwwroot/js/autovehicule.js
@@ -26,8 +26,8 @@ function loadDataTable() {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                      <a href = "/admin/autovehicule/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Edit> </a>
-                      <a onClick = Delete('/admin/autovehicule/delete/${data}') class="btn btn-danger mx-2">  <i class="bi bi-trash3"></i>Delete </a>
+                      <a href = "/admin/autovehicule/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit </a>
+                      <a onClick = Delete('/admin/autovehicule/delete/${data}') class="btn btn-danger mx-2">  <i class="bi bi-trash3"></i> Delete </a>
                            </div>`
                 },
                 "width": "10%"
